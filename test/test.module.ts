422589b9b996f6ddd916fbab29f7eb7ee805c854
thomasalbertoDev <thomasalberto456@gmail.com>
2024-06-27 11:42:28 +0700

    test: Users API testing completed

--- test/test.module.ts
+++ test/test.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersTestService } from './users/usersTest.service';
 
 @Module({
-  providers: [UsersTestService],
+  providers: [],
 })
 export class TestModule {}
--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,14 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
 import { TestModule } from '../test.module';
 import { INestApplication } from '@nestjs/common';
-import { UsersTestService } from './usersTest.service';
 import { Test, TestingModule } from '@nestjs/testing';
 
 describe('TESTING_USERS_API', () => {
   let app: INestApplication;
-  let usersTestService: UsersTestService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -17,10 +14,9 @@ describe('TESTING_USERS_API', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
-
-    usersTestService = app.get(UsersTestService);
   });
 
+  // SignUp API Testing
   describe('TESTING_USERS_API_001', () => {
     // Scenario 1
     it('should return 201 if user is created successfully!', async () => {
@@ -86,6 +82,193 @@ describe('TESTING_USERS_API', () => {
         });
 
       expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
+
+  // SignIn API Testing
+  describe('TESTING_USERS_API_002', () => {
+    // Scenario 1
+    it('should return 201 if user is login successfully!', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users/signin')
+        .send({
+          username: 'admin1',
+          password: 'admin1234',
+        });
+
+      expect(response.status).toBe(201);
+      expect(response.body.data.accessToken).toBeDefined();
+    });
+
+    // Scenario 2
+    it('should return 401 if username invalid & password valid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users/signin')
+        .send({
+          username: 'admin312',
+          password: 'admin123',
+        });
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 3
+    it('should return 401 if username valid & password invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users/signin')
+        .send({
+          username: 'admin1',
+          password: 'admin123',
+        });
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 4
+    it('should return 401 if username invalid & password invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users/signin')
+        .send({
+          username: 'admin312',
+          password: 'admin1234',
+        });
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
+
+  // Get User By Token API Testing
+  describe('TESTING_USERS_API_003', () => {
+    // Scenario 1
+    it('should return 200 if token valid & token found', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/users')
+        .set(
+          'Authorization',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViYzcxZjdlLWQzYmMtNDEzYy1iODdhLWRiMjMyNGU0Y2RjOSIsInVzZXJuYW1lIjoiYWRtaW4xIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzE5NDYwMzczLCJleHAiOjE3MTk1NDY3NzN9.SVmsOz2IGr2YeOgPIFdTl2rjKlYlhw7N_S3q3EtPi04',
+        );
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.id).toBeDefined();
+      expect(response.body.data.username).toBeDefined();
+      expect(response.body.data.role).toBeDefined();
+    });
+
+    // Scenario 2
+    it('should return 401 if token invalid & token found', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/users')
+        .set('Authorization', 'Bearer test');
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 3
+    it('should return 401 if token not found', async () => {
+      const response = await request(app.getHttpServer()).get('/api/users');
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
+
+  // Update Password API Testing
+  describe('TESTING_USERS_API_004', () => {
+    // Scenario 1
+    it('should return 200 if update password successfully and token found!', async () => {
+      const response = await request(app.getHttpServer())
+        .put('/api/users/update-password')
+        .set(
+          'Authorization',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViYzcxZjdlLWQzYmMtNDEzYy1iODdhLWRiMjMyNGU0Y2RjOSIsInVzZXJuYW1lIjoiYWRtaW4xIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzE5NDYxMjc0LCJleHAiOjE3MTk1NDc2NzR9.SP123jMMCjOc0ao8wtYfBwaEr5UzPkPDuYTP8RnBKhk',
+        )
+        .send({
+          password: 'admin1234',
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBeDefined();
+    });
+
+    // Scenario 2
+    it('should return 400 if password characters are less than 8 and token found!', async () => {
+      const response = await request(app.getHttpServer())
+        .put('/api/users/update-password')
+        .set(
+          'Authorization',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViYzcxZjdlLWQzYmMtNDEzYy1iODdhLWRiMjMyNGU0Y2RjOSIsInVzZXJuYW1lIjoiYWRtaW4xIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzE5NDYxMjc0LCJleHAiOjE3MTk1NDc2NzR9.SP123jMMCjOc0ao8wtYfBwaEr5UzPkPDuYTP8RnBKhk',
+        )
+        .send({
+          password: 'test',
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 3
+    it('should return 401 if password characters valid & token invalid!', async () => {
+      const response = await request(app.getHttpServer())
+        .put('/api/users/update-password')
+        .set('Authorization', 'Bearer test')
+        .send({
+          password: 'admin1234',
+        });
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 4
+    it('should return 401 if password characters valid & token not found!', async () => {
+      const response = await request(app.getHttpServer())
+        .put('/api/users/update-password')
+        .send({
+          password: 'admin1234',
+        });
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
+
+  // Logout API Testing
+  describe('TESTING_USERS_API_005', () => {
+    // Scenario 1
+    it('should return 200 if logout successfully & token found', async () => {
+      const response = await request(app.getHttpServer())
+        .delete('/api/users/signout')
+        .set(
+          'Authorization',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViYzcxZjdlLWQzYmMtNDEzYy1iODdhLWRiMjMyNGU0Y2RjOSIsInVzZXJuYW1lIjoiYWRtaW4xIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzE5NDYxMjc0LCJleHAiOjE3MTk1NDc2NzR9.SP123jMMCjOc0ao8wtYfBwaEr5UzPkPDuYTP8RnBKhk',
+        );
+
+      expect(response.status).toBe(200);
+      expect(response.body.message).toBeDefined();
+    });
+
+    // Scenario 2
+    it('should return 401 if token invalid & token found', async () => {
+      const response = await request(app.getHttpServer())
+        .delete('/api/users/signout')
+        .set('Authorization', 'Bearer test');
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 3
+    it('shoud return 401 if token not found', async () => {
+      const response = await request(app.getHttpServer()).delete(
+        '/api/users/signout',
+      );
+
+      expect(response.status).toBe(401);
       expect(response.body.errors).toBeDefined();
     });
   });
