daea73a766af988e8141fefd0fd86ac91c9d1475
thomasalbertoDev <thomasalberto456@gmail.com>
2024-07-05 19:52:55 +0700

    test: Get agama by id testing completed

--- test/master/agama/agama.spec.ts
+++ test/master/agama/agama.spec.ts
@@ -128,4 +128,53 @@ describe('TESTING_AGAMA_API', () => {
       expect(response.body.errors).toBeDefined();
     });
   });
+
+  // Get agama by id API testing
+  describe('TESTING_AGAMA_API_003', () => {
+    // Scenario 1 : Should retrun 200 if get agama by id success
+    it('should return 200 if get agama by id success!', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/master/agama/31ed04b0-6fd3-42fb-9616-9e4da4ac47f4')
+        .set(
+          'Authorization',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViYzcxZjdlLWQzYmMtNDEzYy1iODdhLWRiMjMyNGU0Y2RjOSIsInVzZXJuYW1lIjoiYWRtaW4xIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzIwMTgwNzI2LCJleHAiOjE3MjAyNjcxMjZ9.4yV1mDnkYYMPNSAH6jUF6D--870rt2HHk2blQn7EvOw',
+        );
+
+      expect(response.status).toBe(200);
+      expect(response.body.data).toBeDefined();
+    });
+
+    // Scenario 2 : Should return 404 if agama not found & valid token
+    it('should return 404 if agama not found & valid token!', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/master/agama/test')
+        .set(
+          'Authorization',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjViYzcxZjdlLWQzYmMtNDEzYy1iODdhLWRiMjMyNGU0Y2RjOSIsInVzZXJuYW1lIjoiYWRtaW4xIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzIwMTgwNzI2LCJleHAiOjE3MjAyNjcxMjZ9.4yV1mDnkYYMPNSAH6jUF6D--870rt2HHk2blQn7EvOw',
+        );
+
+      expect(response.status).toBe(404);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 3 : Should return 401 if token not found
+    it('should return 401 if token not found!', async () => {
+      const response = await request(app.getHttpServer()).get(
+        '/api/master/agama/31ed04b0-6fd3-42fb-9616-9e4da4ac47f4',
+      );
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    // Scenario 4 : Should return 401 if token invalid
+    it('should return 401 if token invalid!', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/api/master/agama/31ed04b0-6fd3-42fb-9616-9e4da4ac47f4')
+        .set('Authorization', 'test');
+
+      expect(response.status).toBe(401);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
 });
